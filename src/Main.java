5cf34db9d9f8eea60cfb8122517e4d6c5f43963c
Roman Wysmyk <wysmykroman@gmail.com>
2020-02-04 21:59:46 +0100

    Implements randomEventGenerator 0.5ver

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
         Auto auto = new Auto();
         Droga droga = new Droga();
+        RoadEvents events = new RoadEvents();
 
-        auto.refuelCar(10);
+        /*auto.refuelCar(10);
         auto.turnEngineOnOff();
-        auto.drive(droga);
+        auto.drive(droga);*/
+
+        events.eventRandomizer(auto);
 
     }
 }
--- src/RoadEvents.java
+++ src/RoadEvents.java
@@ -1,4 +1,6 @@
-public class RoadEvents {
+import java.util.Random;
+
+public class RoadEvents implements Interface {
 
 
     void roadHelpwithTire(Auto auto) {
@@ -8,30 +10,56 @@ public class RoadEvents {
         System.out.println("Masz teraz " + auto.getCurrentMoney() + " gotówki");
     }
 
-    void roadLuckt(Droga droga) {
+    void roadLuck() {
 
         System.out.println("Zobaczyłeś tęczę !");
 
     }
 
-    /*void eventRandomizer(Auto auto){
-        this.auto = auto;
-        Random randomEvent = new Random();
+    void streetDemon(Auto auto) {
+
+        System.out.println("Otrzymujesz mandat za szybką jazdę. Musisz zapłacić 20 $ grzywny");
+
+        auto.setCurrentMoney(-20);
+        System.out.println("Pozostało Ci " + auto.getCurrentMoney() + " pieniędzy");
 
 
-        if (randomEvent.nextInt(3)==0){
-            void streetDemon(auto){
+    }
 
-                System.out.println("Otrzymujesz mandat za szybką jazdę. Musisz zapłacić 20 $ grzywny");
+    void eventRandomizer(Auto auto) {
 
-                auto.setCurrentMoney(-20);
-                System.out.println("Pozostało Ci "+auto.getCurrentMoney()+" pieniędzy");
+        Random eventRandomizer = new Random();
+        int x = eventRandomizer.nextInt(2);
 
+        if (x == 0) {
 
+            class Events {
+
+                void TESTT(Auto auto) {
+                    auto.setCurrentMoney(1000);
+                    System.out.println(auto.getCurrentMoney());
+                    System.out.println("metoda numer 0");
+                }
 
             }
-            System.out.println("lol");
-        }
+            new Events().TESTT(auto);
 
-    }*/
+
+        } else if (x == 1) {
+
+            class Events {
+                void TESTT1(Auto auto) {
+                    auto.setCurrentMoney(2000);
+                    System.out.println(auto.getCurrentMoney());
+                    System.out.println("metoda numer 1");
+                }
+            }
+            new Events().TESTT1(auto);
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
